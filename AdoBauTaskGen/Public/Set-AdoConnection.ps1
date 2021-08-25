b217940dfc3607a51e1c73ca62ec4b34e62c2704
Adam Barkes <adam.barkes@vanquisbank.co.uk>
2021-08-25 12:28:45 +0100

    feat: add set-adoconnection function

---
+++ AdoBauTaskGen/Public/Set-AdoConnection.ps1
@@ -0,0 +1,19 @@
+function Set-AdoConnection {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Url,
+
+        [Parameter(Mandatory)]
+        [string]
+        $Project,
+
+        [Parameter(Mandatory)]
+        [string]
+        $PersonalAccessToken
+    )
+    
+    Set-VSTeamAccount -Account $Url -PersonalAccessToken $PersonalAccessToken
+    Set-VSTeamDefaultProject -Project $Project
+}
