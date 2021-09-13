ccb94b93b2afe41f50400a67d0f44ffc5536d1a9
= <=>
2021-09-13 12:33:52 +0100

    test(unit test): set-adoconnection parameter tests

--- Helpers/Set-TestHelpersEnvVars.ps1
+++ Helpers/Set-TestHelpersEnvVars.ps1
@@ -3,3 +3,5 @@ param (
 )
 
 Set-BuildEnvironment -Path ($Path -replace "Tests.+" -replace "Tools") -Force
+$ENV:THFunctionName = (Get-Item -Path $Path).BaseName -replace "\.tests"
+$ENV:THFunctionPath = $Path -replace ".+Unit-Tests",$ENV:BHPSModulePath -replace "\.tests"
---
+++ Tests/Unit-Tests/Public/Set-AdoConnection.tests.ps1
@@ -0,0 +1,55 @@
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppress false positives in BeforeAll scriptblock')]
+param()
+
+$BuildVarModuleFilePath = Join-Path -Path ($PSScriptRoot -replace "Tests.+") -ChildPath "Helpers" -AdditionalChildPath "Set-TestHelpersEnvVars.ps1"
+. $BuildVarModuleFilePath -Path $PSCommandPath
+
+Describe "$ENV:THFunctionName" {
+
+    BeforeAll {
+        $FunctionName = $ENV:THFunctionName
+        . $ENV:THFunctionPath
+
+        $FuncDependancies = @{
+            Private = @()
+            Public = @()
+            Classes = @()
+        }
+        foreach ($Scope in $FuncDependancies.Keys) {
+            foreach ($Function in $FuncDependancies.$Scope) {
+                $FuncPath = Join-Path -Path $ENV:BHPSModulePath -ChildPath $Scope -AdditionalChildPath "$Function.ps1"
+                # Import dependant function into to scope
+                . $FuncPath
+            }
+        }
+    }
+
+    Context "Parameters" {
+        It "Does not contain untested parameters" {
+            $ParamInfo = (Get-Command $FunctionName).Parameters
+            $ParamInfo.Count -13 | Should -Be 3
+        }
+
+        It "Has a mandatory Url parameter" {
+            $ParamInfo = (Get-Command $FunctionName).Parameters["Url"]
+            $ParamInfo | Should -Not -BeNullOrEmpty
+            $ParamInfo.ParameterType.ToString() | Should -Be "System.String"
+            $ParamInfo.Attributes.Mandatory | Should -Be $true
+        }
+
+        It "Has a mandatory Project parameter" {
+            $ParamInfo = (Get-Command $FunctionName).Parameters["Project"]
+            $ParamInfo | Should -Not -BeNullOrEmpty
+            $ParamInfo.ParameterType.ToString() | Should -Be "System.String"
+            $ParamInfo.Attributes.Mandatory | Should -Be $true
+        }
+        
+        It "Has a mandatory PersonalAccessToken parameter" {
+            $ParamInfo = (Get-Command $FunctionName).Parameters["PersonalAccessToken"]
+            $ParamInfo | Should -Not -BeNullOrEmpty
+            $ParamInfo.ParameterType.ToString() | Should -Be "System.String"
+            $ParamInfo.Attributes.Mandatory | Should -Be $true
+        }
+    }
+
+}
