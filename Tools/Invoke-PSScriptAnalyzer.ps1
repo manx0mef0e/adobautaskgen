d9f68a53b70e9a2ec633de2c388a92d36b959b57
= <=>
2021-09-14 11:41:00 +0100

    refactor: use childpath instead of seperate line
    Co-authored-by: Tony <tonylea@users.noreply.github.com>

--- Tools/Invoke-PSScriptAnalyzer.ps1
+++ Tools/Invoke-PSScriptAnalyzer.ps1
@@ -1,6 +1,5 @@
 # This will run via PScriptAnalyzer on the module files.
-$BuildVarModuleFilePath = Join-Path -Path ($PSScriptRoot -replace "Tools") -ChildPath "Helpers"
-$BuildVarModuleFilePath = Join-Path -Path $BuildVarModuleFilePath -ChildPath "Set-TestHelpersEnvVars.ps1"
+$BuildVarModuleFilePath = Join-Path -Path ($PSScriptRoot -replace "Tools") -ChildPath "Helpers" -ChildPath "Set-TestHelpersEnvVars.ps1"
 . $BuildVarModuleFilePath -Path $PSScriptRoot
 
 $Result = Invoke-ScriptAnalyzer -Path $ENV:BHPSModulePath -Severity @("Error","Warning") -Recurse
--- Tools/Run-UnitTests.ps1
+++ Tools/Run-UnitTests.ps1
@@ -1,8 +1,7 @@
 # This will run Pester Unit Tests
-$BuildVarModuleFilePath = Join-Path -Path ($PSScriptRoot -replace "Tools") -ChildPath "Helpers"
-$BuildVarModuleFilePath = Join-Path -Path $BuildVarModuleFilePath -ChildPath "Set-TestHelpersEnvVars.ps1"
+$BuildVarModuleFilePath = Join-Path -Path ($PSScriptRoot -replace "Tools") -ChildPath "Helpers" -ChildPath "Set-TestHelpersEnvVars.ps1"
 . $BuildVarModuleFilePath -Path $PSScriptRoot
 
 $UnitTestPath = Join-Path -Path ($PSScriptRoot -replace "Tools") -ChildPath "Tests" -AdditionalChildPath "Unit-Tests"
 
-Invoke-Pester -Path $UnitTestPath
+Invoke-Pester -Path $UnitTestPath -Output Detailed
