4da3c104d0ffb22a539bcaa19f7ffb676173434f
= <=>
2021-09-14 13:45:22 +0100

    fix: correct syntax error on join-path

--- Tools/Run-UnitTests.ps1
+++ Tools/Run-UnitTests.ps1
@@ -1,5 +1,5 @@
 # This will run Pester Unit Tests
-$BuildVarModuleFilePath = Join-Path -Path ($PSScriptRoot -replace "Tools") -ChildPath "Helpers" -ChildPath "Set-TestHelpersEnvVars.ps1"
+$BuildVarModuleFilePath = Join-Path -Path ($PSScriptRoot -replace "Tools") -ChildPath "Helpers" -AdditionalChildPath "Set-TestHelpersEnvVars.ps1"
 . $BuildVarModuleFilePath -Path $PSScriptRoot
 
 $UnitTestPath = Join-Path -Path ($PSScriptRoot -replace "Tools") -ChildPath "Tests" -AdditionalChildPath "Unit-Tests"
