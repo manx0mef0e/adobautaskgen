0a47288870bfa9694dc6a6a585727c52c2f17d0a
= <=>
2021-09-27 11:29:54 +0100

    chore: line endings changed
    Co-authored-by: Tony <tonylea@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # adobautaskgen
-A simple module to create opinionated tasks in Azure DevOps 
+
+A simple module to create opinionated tasks in Azure DevOps
+
+
+https://kapeli.com/cheat_sheets/Conventional_Commits.docset/Contents/Resources/Documents/index
+
+
+windows LRCR
+
+nix CR
---
+++ src/Private/ConvertFrom-MarkdownToHtml.ps1
@@ -0,0 +1,21 @@
+function ConvertFrom-MarkdownToHtml {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Markdown
+    )
+
+    $Uri = "https://api.github.com/markdown"
+    $ApiRequestBody = New-Object -TypeName PSObject
+    $ApiRequestBody | Add-Member -MemberType NoteProperty -Name text -Value $Markdown
+    $ApiRequestBody | Add-Member -MemberType NoteProperty -Name mode -Value "markdown"
+
+    $Response = Invoke-WebRequest -Uri $Uri -Method Post -Body ($ApiRequestBody | ConvertTo-Json)
+
+
+    if ($Response.StatusCode -eq 200) {
+        return $Response.Content
+    }
+    # TODO: What happens when we don't get the 200 response?
+}
