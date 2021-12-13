fd17dbaece0712ceb75e2880b150d3c23177d073
Adam Barkes <sekrab@gmail.com>
2021-12-13 12:01:29 +0000

    test: add tests for ado-bautasks

--- Tests/Unit-Tests/Public/Add-AdoBauTask.tests.ps1
+++ Tests/Unit-Tests/Public/Add-AdoBauTask.tests.ps1
@@ -59,6 +59,66 @@ Describe "Add-AdoBauTask" {
     }
 
     Context "Core functionality" {
+        BeforeEach {
+            Mock Get-VSTeamWorkItem {
+                return @{
+                    AreaPath = "MockAreaPath"
+                    IterationPath = "MockIterationPath"
+                    ProjectName = "MockProjectName"
+                }
+            }
+            Mock ConvertFrom-MarkdownToHtml {
+                return "MockHtmlDescription"
+            }
+            Mock Get-FullTitle {
+                return "MockTitle"
+            }
+            Mock Add-VSTeamWorkItem
+
+            Add-AdoBauTask -Title "Title" -Description "Description" -ParentID 69 -Frequency "Daily"
+        }
+
+        It "Gets the work item details" {
+            Should -Invoke -CommandName Get-VSTeamWorkItem
+        }
+
+        It "Should convert Markdown to HTML" {
+            Should -Invoke -CommandName ConvertFrom-MarkdownToHtml
+        }
+
+        It "Should get the Full Title" {
+            Should -Invoke -CommandName Get-FullTitle
+        }
+
+        It "Should create a new task" {
+            Should -Invoke -CommandName Add-VSTeamWorkItem
+        }
 
     }
+
+    Context "Task creation with set run date" {
+        BeforeEach {
+            Mock Get-VSTeamWorkItem {
+                return @{
+                    AreaPath = "MockAreaPath"
+                    IterationPath = "MockIterationPath"
+                    ProjectName = "MockProjectName"
+                }
+            }
+            Mock ConvertFrom-MarkdownToHtml {
+                return "MockHtmlDescription"
+            }
+            Mock Get-FullTitle {
+                return "MockTitle"
+            }
+            Mock Add-VSTeamWorkItem
+
+            Add-AdoBauTask -Title "Title" -Description "Description" -ParentID 69 -RunDate 10
+        }
+
+        It "Uses the specific run date in setting task name" {
+            Should -Invoke -CommandName Get-FullTitle -ParameterFilter {$RunDate -eq 10}
+        }
+    }
+
 }
